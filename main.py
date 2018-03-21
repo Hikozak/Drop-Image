cd6bf303c87330847e82e3d57f9d300e0cb23663
edge24 <emmanuel.emit@gmail.com>
2018-03-21 09:32:12 +0800

    Navigation stuff, tried php but failed

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, request, send_from_directory, render_template
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_folder='', static_url_path='')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def home():
+    return render_template('about.html')
+
+@app.route('/upload.php')
+def upload():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 @app.route('/js/<path:path>')
 def send_js(path):
